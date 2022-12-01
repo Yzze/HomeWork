225c422d3c560d992e96da45c6f8d4b1de1a96c9
Yzee <yan.zhigurs@mail.ru>
2022-12-01 19:26:41 +0400

    delete comments in ViewController

--- HomeWork/ViewController.swift
+++ HomeWork/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  HomeWork
-//
-//  Created by Ян Жигурс on 01.12.2022.
-//
 
 import UIKit
 
@@ -11,7 +5,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
